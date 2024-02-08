a6aab9e7a939d7f60fb6d266c6dcf9ddb3edd5f4
yanlong <yanlong@ucla.edu>
2024-02-07 23:43:28 -0800

    quick update for loaders

--- tools/fileloader.py
+++ tools/fileloader.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import torch
 import cv2
-from matplotlib import image as mpimg
+from matplotlib import pyplot as plt
 
 
 def load_files(folder_path):
@@ -25,16 +25,28 @@ def load_files(folder_path):
     return file_paths
 
 
-def load_images(folder_path):
-    full_path = os.path.expanduser(folder_path)
-    if not os.path.exists(full_path):
-        raise FileNotFoundError(f'File {full_path} does not exist!')
-
-
 def load_poses(file_path):
+    """
+    Load the positions file (.txt or .npz).
+
+    Args:
+        file_path: (string) the path of the file containing the positions
+    Returns:
+        positions: (np.array) the positions in shape (n, 3, 4).
+    """
     full_path = os.path.expanduser(file_path)
     if not os.path.exists(full_path):
         raise FileNotFoundError(f'File {full_path} does not exist!')
+
+    _, ext = os.path.splitext(full_path)
+    if ext == '.txt':
+        poses = np.loadtxt(file_path, delimiter=' ', dtype=np.float32).reshape(-1, 3, 4)
+    elif ext == '.npz':
+        poses = np.load(full_path, allow_pickle=True)['arr_0'].astype(np.float32)
+        # poses = np.load(full_path, allow_pickle=True)['poses'].astype(np.float32)
+    else:
+        raise TypeError(f'File {full_path} cannot read!')
+    return poses
 
 
 def load_descriptors(file_path):
@@ -50,16 +62,25 @@ def load_labels(file_path):
 
 
 def read_image(image_path):
+    """
+    Read a single image in tensor form.
+
+    Args:
+        image_path: (string) the path of the image
+    Returns:
+        depth_data: (tensor) image tensor in shape [1, 1, H, W]
+    """
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    depth_data = torch.from_numpy(img).type(torch.float32).to(device)
-    depth_data = torch.unsqueeze(depth_data, dim=0)
-    depth_data = torch.unsqueeze(depth_data, dim=0)
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)                    # in grayscale, shape (H, W)
+    depth_data = torch.from_numpy(image).type(torch.float32).to(device)
+    depth_data = torch.unsqueeze(depth_data, dim=0)                         # shape (1, H, W)
+    depth_data = torch.unsqueeze(depth_data, dim=0)                         # shape (1, 1, H, W)
     return depth_data
 
 
 if __name__ == '__main__':
     file_path = '/media/vectr/T9/Dataset/overlap_transformer/npy_files/bomb_shelter'
     img_path = '/media/vectr/T9/Dataset/overlap_transformer/png_files/bomb_shelter/depth/000000.png'
-    file_paths = load_files(file_path)
-    img = read_image(img_path)
+    poses_path = '/media/vectr/T9/Dataset/overlap_transformer/poses/bomb_shelter/poses.txt'
+    load_poses(poses_path)
+
