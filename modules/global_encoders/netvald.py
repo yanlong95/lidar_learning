3fa2ab86d69bd639f1410f08297716c8185c95bc
yanlong <yanlong@ucla.edu>
2024-02-14 23:10:36 -0800

    quick update for global descriptors

--- modules/global_encoders/netvald.py
+++ modules/global_encoders/netvald.py
@@ -1,4 +1,8 @@
-import os
+"""
+Code taken from https://github.com/cattaneod/PointNetVlad-Pytorch/blob/master/models/PointNetVlad.py with trivial
+modifications
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,7 +11,7 @@ from math import sqrt
 
 class NetVLADLoupe(nn.Module):
     """
-    The netvald neural network, details check the paper NetVLAD and paper PointNetVLAD.
+    The NetVLAD neural network, details check the paper NetVLAD and paper PointNetVLAD.
     """
     def __init__(self, feature_size, max_samples, cluster_size, output_dim,
                  gating=True, add_batch_norm=True, is_training=True):
@@ -22,9 +26,12 @@ class NetVLADLoupe(nn.Module):
         self.softmax = nn.Softmax(dim=-1)
 
         # add weights variables
-        self.cluster_weights = nn.Parameter(1 / sqrt(feature_size) * torch.randn(feature_size, cluster_size))
-        self.cluster_weights2 = nn.Parameter(1 / sqrt(feature_size) * torch.randn(1, feature_size, cluster_size))
-        self.hidden_weights = nn.Parameter(1 / sqrt(feature_size) * torch.randn(feature_size * cluster_size, output_dim))
+        self.cluster_weights = nn.Parameter(1 / sqrt(feature_size) *
+                                            torch.randn(feature_size, cluster_size))                   # parameters w
+        self.cluster_weights2 = nn.Parameter(1 / sqrt(feature_size) *
+                                             torch.randn(1, feature_size, cluster_size))               # parameters c_k
+        self.hidden_weights = nn.Parameter(1 / sqrt(feature_size) *
+                                           torch.randn(feature_size * cluster_size, output_dim))       # output weights
 
         # determine cluster norm and biases
         if add_batch_norm:
@@ -42,7 +49,7 @@ class NetVLADLoupe(nn.Module):
 
     def forward(self, x):
         """
-        feedforward pass of the netvald neural network
+        feedforward pass of the NetVLAD neural network
 
         Args:
             x: (torch.tensor) with dimension (batch_size, feature_size, max_samples, 1).
@@ -50,7 +57,43 @@ class NetVLADLoupe(nn.Module):
                 D dimensional descriptors extracted at H * W spatial locations.
                 Here H = 1, W = max_samples, D = feature_size.
         """
-        pass
+
+        x = x.transpose(1, 3).contiguous()
+        x = x.view(-1, self.max_samples, self.feature_size)
+
+        # soft-assignment branch
+        activation = x @ self.cluster_weights
+        if self.add_batch_norm:
+            activation = activation.view(-1, self.cluster_size)
+            activation = self.bn1(activation)
+            activation = activation.view(-1, self.max_samples, self.cluster_size)
+        else:
+            activation += self.cluster_biases
+        activation = self.softmax(activation)
+        # activation = activation.view(-1, self.max_samples, self.cluster_size)   # seems redundant here
+
+        a_sum = activation.sum(-2, keepdim=True)
+        a = a_sum * self.cluster_weights2
+
+        # VLAD core
+        # x = x.view(-1, self.max_samples, self.feature_size)     # seems redundant here
+        activation = activation.transpose(2, 1)
+        vlad = activation @ x
+        vlad = vlad.transpose(2, 1)
+        vlad -= a
+
+        # reform the output as a 1D vector
+        vlad = F.normalize(vlad, p=2, dim=1)
+        vlad = vlad.reshape(-1, self.feature_size * self.cluster_size)
+        vlad = F.normalize(vlad, p=2, dim=1)
+        vlad = vlad @ self.hidden_weights
+        vlad = self.bn2(vlad)
+
+        # apply gatingContext filter
+        if self.gating:
+            vlad = self.context_gating(vlad)
+
+        return vlad
 
 
 class GatingContext(nn.Module):
@@ -60,10 +103,10 @@ class GatingContext(nn.Module):
 
     def __init__(self, dim, add_batch_norm=True):
         """
-        Define a small filter to determine if the weight of each element in the netvald descriptor.
+        Define a small filter to determine if the weight of each element in the NetVLAD descriptor.
 
         Args:
-            dim: (int) the dimension of the descriptor output by netvald.
+            dim: (int) the dimension of the descriptor output by NetVLAD.
             add_batch_norm: (bool) whether to use batch normalization (if not add biases).
         """
         super().__init__()
@@ -94,32 +137,22 @@ class GatingContext(nn.Module):
 
 
 if __name__ == '__main__':
-    # net_vlad = NetVLADLoupe(feature_size=1024, max_samples=900, cluster_size=64,
-    #                         output_dim=256, gating=True, add_batch_norm=False,
-    #                         is_training=True)
-
-    net_vlad = NetVLADLoupe(feature_size=1024, max_samples=900, cluster_size=16,
-                            output_dim=20, gating=True, add_batch_norm=True,
+    net_vlad = NetVLADLoupe(feature_size=1024, max_samples=900, cluster_size=64,
+                            output_dim=256, gating=True, add_batch_norm=True,
                             is_training=True)
-    # input  (bs, 1024, 360, 1)
-    torch.manual_seed(1234)
-    input_tensor = F.normalize(torch.randn((2, 1024, 900, 1)), dim=1)
+    # input  (bs, 1024, 900, 1)
+    torch.manual_seed(0)
+    input1 = F.normalize(torch.randn((1, 1024, 900, 1)), dim=1)
+    input2 = input1.clone()
+    input2 = input2[:, :, torch.randperm(input2.shape[2]), :]
+    diff = torch.norm(input2 - input1)
+
     print("==================================")
+    print(f'Norm of the difference before NetVLAD: {diff:.3f}.')
 
     with torch.no_grad():
         net_vlad.eval()
-        print(input_tensor.shape)
-        # out1 = net_vlad(input_tensor)
-        # print(out1.shape)
-        # net_vlad.eval()
-        # # input_tensor2[:, :, 20:, :] = 0.1
-        # input_tensor2 = F.normalize(input_tensor2, dim=1)
-        # out2 = net_vlad(input_tensor2)
-        # print(out2)
-        # net_vlad.eval()
-        # input_tensor3 = torch.randn((1, 1024, 360, 1))
-        # out3 = net_vlad(input_tensor3)
-        # print(out3)
-        #
-        # print(((out1 - out2) ** 2).sum(1))
-        # print(((out1 - out3) ** 2).sum(1))
+        output1 = net_vlad(input1)
+        output2 = net_vlad(input2)
+        diff = torch.norm(output2 - output1)
+        print(f'Norm of the difference after NetVLAD: {diff:.3f}.')
